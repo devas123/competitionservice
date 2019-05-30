30c13be376eb64742201f6bc978882ba2f1a8f47
ggrigori <grigoriy.grigoriev@t-systems.com>
2019-05-30 20:19:47 +0300

    updated mapper configuration for scalecube-cluster

--- competition-serv-service/src/main/java/compman/compsrv/json/ObjectMapperFactory.kt
+++ competition-serv-service/src/main/java/compman/compsrv/json/ObjectMapperFactory.kt
@@ -1,7 +1,10 @@
 package compman.compsrv.json
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect
 import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.annotation.JsonTypeInfo
+import com.fasterxml.jackson.annotation.PropertyAccessor
+import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module
@@ -10,12 +13,22 @@ import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import com.fasterxml.jackson.module.paramnames.ParameterNamesModule
 
 object ObjectMapperFactory {
-    fun createObjectMapper(): ObjectMapper = ObjectMapper()
-            .registerKotlinModule()
-            .registerModule(ParameterNamesModule())
-            .registerModule(Jdk8Module())
-            .registerModule(JavaTimeModule())
-            .setDefaultPropertyInclusion(JsonInclude.Include.NON_NULL)
-            .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
-            .enableDefaultTyping(ObjectMapper.DefaultTyping.OBJECT_AND_NON_CONCRETE, JsonTypeInfo.As.PROPERTY)
+    fun createObjectMapper(): ObjectMapper {
+        val mapper = ObjectMapper()
+                .registerKotlinModule()
+                .registerModule(ParameterNamesModule())
+                .registerModule(Jdk8Module())
+                .registerModule(JavaTimeModule())
+                .setDefaultPropertyInclusion(JsonInclude.Include.NON_NULL)
+                .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+                .enableDefaultTyping(ObjectMapper.DefaultTyping.OBJECT_AND_NON_CONCRETE, JsonTypeInfo.As.PROPERTY)
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+        mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false)
+        mapper.configure(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_AS_NULL, true)
+        mapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY)
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL)
+        mapper.configure(SerializationFeature.WRITE_ENUMS_USING_TO_STRING, true)
+        mapper.enableDefaultTyping(ObjectMapper.DefaultTyping.JAVA_LANG_OBJECT, JsonTypeInfo.As.PROPERTY)
+        return mapper
+    }
 }
