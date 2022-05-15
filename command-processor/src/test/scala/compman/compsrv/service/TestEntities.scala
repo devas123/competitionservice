184d9d019abf2693c7a7e9bcac64016feb19b455
ggrigori <grigoriy.grigoriev@gmail.com>
2022-05-15 13:15:56 +0100

    WIP: Started fixing tests

--- command-processor/src/test/scala/compman/compsrv/service/TestEntities.scala
+++ command-processor/src/test/scala/compman/compsrv/service/TestEntities.scala
@@ -1,177 +1,160 @@
 package compman.compsrv.service
 
-import compman.compsrv.model.dto.brackets._
-import compman.compsrv.model.dto.competition._
-import compman.compsrv.model.dto.dashboard.MatDescriptionDTO
-
-import scala.jdk.CollectionConverters.CollectionHasAsScala
+import compman.compsrv.logic.fight.FightResultOptionConstants
+import compservice.model.protobuf.model._
 
 trait TestEntities {
-  final val fight2 = "fight2"
-  final val fight1 = "fight1"
-  final val fight3 = "fight3"
-  final val mat1Id = "mat1"
-  final val mat2Id = "mat2"
+  final val fight2        = "fight2"
+  final val fight1        = "fight1"
+  final val fight3        = "fight3"
+  final val mat1Id        = "mat1"
+  final val mat2Id        = "mat2"
   final val competitionId = "managedCompetition"
-  final val categoryId = "test-category"
-  final val stageId = "stage-id"
+  final val categoryId    = "test-category"
+  final val stageId       = "stage-id"
   final val matId: String = "mat-id"
-  final val periodId = "period-id"
+  final val periodId      = "period-id"
 
-  final val groupIdFormat = "group-id-%s"
-  final val groupRange: Range.Inclusive = 0 to 3
+  final val groupIdFormat                   = "group-id-%s"
+  final val groupRange: Range.Inclusive     = 0 to 3
   final val startingCompetitorsSizeForGroup = 5
 
-  val groupDescriptors: IndexedSeq[GroupDescriptorDTO] = groupRange map { groupIndex =>
-    new GroupDescriptorDTO().setId(groupIdFormat.format(groupIndex)).setName(groupIdFormat.format(groupIndex))
-      .setSize(startingCompetitorsSizeForGroup + groupIndex)
+  val groupDescriptors: IndexedSeq[GroupDescriptor] = groupRange map { groupIndex =>
+    GroupDescriptor().withId(groupIdFormat.format(groupIndex)).withName(groupIdFormat.format(groupIndex))
+      .withSize(startingCompetitorsSizeForGroup + groupIndex)
   }
 
-  val testMat: MatDescriptionDTO = new MatDescriptionDTO()
-    .setId(matId)
-    .setName("Test mat")
-    .setPeriodId(periodId)
-    .setMatOrder(0)
+  val testMat: MatDescription = MatDescription().withId(matId).withName("Test mat").withPeriodId(periodId)
+    .withMatOrder(0)
 
   val totalNumberOfCompetitors: Int = groupRange.map(_ + startingCompetitorsSizeForGroup).sum
 
-  val inputDescriptor: StageInputDescriptorDTO = new StageInputDescriptorDTO().setSelectors(Array.empty)
-    .setNumberOfCompetitors(totalNumberOfCompetitors).setDistributionType(DistributionType.AUTOMATIC)
+  val inputDescriptor: StageInputDescriptor = StageInputDescriptor().withSelectors(Array.empty)
+    .withNumberOfCompetitors(totalNumberOfCompetitors).withDistributionType(DistributionType.AUTOMATIC)
 
-  val resultDescriptor: StageResultDescriptorDTO = new StageResultDescriptorDTO().setName(stageId)
-    .setCompetitorResults(Array.empty).setFightResultOptions(FightResultOptionDTO.values.asScala.toArray)
-    .setOutputSize(1)
+  val resultDescriptor: StageResultDescriptor = StageResultDescriptor().withName(stageId)
+    .withCompetitorResults(Array.empty).withFightResultOptions(FightResultOptionConstants.values).withOutputSize(1)
 
-  val stageForGroupsGeneration: StageDescriptorDTO = new StageDescriptorDTO().setId(stageId).setName(stageId)
-    .setCompetitionId(competitionId).setStageType(StageType.FINAL).setStageOrder(0).setBracketType(BracketType.GROUP)
-    .setFightDuration(600).setGroupDescriptors(groupDescriptors.toArray)
-    .setInputDescriptor(inputDescriptor).setStageResultDescriptor(resultDescriptor.setAdditionalGroupSortingDescriptors(Array.empty).setForceManualAssignment(false))
-    .setStageStatus(StageStatus.WAITING_FOR_APPROVAL).setWaitForPrevious(false)
+  val stageForGroupsGeneration: StageDescriptor = StageDescriptor().withId(stageId).withName(stageId)
+    .withCompetitionId(competitionId).withStageType(StageType.FINAL).withStageOrder(0)
+    .withBracketType(BracketType.GROUP).withFightDuration(600).withGroupDescriptors(groupDescriptors)
+    .withInputDescriptor(inputDescriptor).withStageResultDescriptor(
+      resultDescriptor.withAdditionalGroupSortingDescriptors(Array.empty).withForceManualAssignment(false)
+    ).withStageStatus(StageStatus.WAITING_FOR_APPROVAL).withWaitForPrevious(false)
 
-  val singleEliminationBracketsStage: StageDescriptorDTO = new StageDescriptorDTO().setId(stageId).setName(stageId)
-    .setCompetitionId(competitionId).setStageType(StageType.FINAL).setStageOrder(0).setBracketType(BracketType.SINGLE_ELIMINATION)
-    .setFightDuration(600)
-    .setInputDescriptor(inputDescriptor).setStageResultDescriptor(resultDescriptor)
-    .setStageStatus(StageStatus.APPROVED).setWaitForPrevious(false)
+  val singleEliminationBracketsStage: StageDescriptor = StageDescriptor().withId(stageId).withName(stageId)
+    .withCompetitionId(competitionId).withStageType(StageType.FINAL).withStageOrder(0)
+    .withBracketType(BracketType.SINGLE_ELIMINATION).withFightDuration(600).withInputDescriptor(inputDescriptor)
+    .withStageResultDescriptor(resultDescriptor).withStageStatus(StageStatus.APPROVED).withWaitForPrevious(false)
 
-  val competitors = List(
-    new CompetitorDTO().setId("competitor1").setCategories(Array(categoryId)),
-    new CompetitorDTO().setId("competitor2").setCategories(Array(categoryId)),
-    new CompetitorDTO().setId("competitor3").setCategories(Array(categoryId))
+  val competitors: Seq[Competitor] = List(
+    Competitor().withId("competitor1").withCategories(Array(categoryId)),
+    Competitor().withId("competitor2").withCategories(Array(categoryId)),
+    Competitor().withId("competitor3").withCategories(Array(categoryId))
   )
 
-
-  val mat1: MatDescriptionDTO = new MatDescriptionDTO()
-    .setId(mat1Id)
-    .setName("Mat 1")
-    .setMatOrder(0)
-    .setPeriodId(periodId)
-  val mat2: MatDescriptionDTO = new MatDescriptionDTO()
-    .setId(mat2Id)
-    .setName("Mat 2")
-    .setMatOrder(1)
-    .setPeriodId(periodId)
-  val fights = List(
-    new FightDescriptionDTO(
+  val mat1: MatDescription = MatDescription().withId(mat1Id).withName("Mat 1").withMatOrder(0).withPeriodId(periodId)
+  val mat2: MatDescription = MatDescription().withId(mat2Id).withName("Mat 2").withMatOrder(1).withPeriodId(periodId)
+  val fights: Seq[FightDescription] = List(
+    FightDescription(
       fight1,
       categoryId,
-      "Semi-final",
-      fight3,
-      null,
+      Some("Semi-final"),
+      Some(fight3),
+      None,
       Array(
-        new CompScoreDTO(
-          null,
-          "competitor1",
-          new ScoreDTO(0, 0, 0, Array.empty),
+        CompScore(
+          None,
+          Some("competitor1"),
+          Some(Score(0, 0, 0, Array.empty)),
           0,
-          FightReferenceType.PROPAGATED,
-          null
+          Some(FightReferenceType.PROPAGATED),
+          None
         ),
-        new CompScoreDTO(
-          null,
-          "competitor2",
-          new ScoreDTO(0, 0, 0, Array.empty),
+        CompScore(
+          None,
+          Some("competitor2"),
+          Some(Score(0, 0, 0, Array.empty)),
           1,
-          FightReferenceType.PROPAGATED,
-          null
+          Some(FightReferenceType.PROPAGATED),
+          None
         )
       ),
       600,
       0,
-      false,
+      invalid = false,
       StageRoundType.WINNER_BRACKETS,
       FightStatus.PENDING,
-      null,
+      None,
       mat2,
-      null,
+      None,
       0,
       competitionId,
       periodId,
-      null,
+      None,
       stageId,
-      null,
-      null,
+      None,
+      None,
       1
     ),
-    new FightDescriptionDTO(
+    FightDescription(
       fight2,
       categoryId,
-      "Semi-final",
-      fight3,
-      null,
+      Some("Semi-final"),
+      Some(fight3),
+      None,
       Array(
-        new CompScoreDTO(
-          null,
-          "competitor3",
-          new ScoreDTO(0, 0, 0, Array.empty),
+        CompScore(
+          None,
+          Some("competitor3"),
+          Some(Score(0, 0, 0, Array.empty)),
           0,
-          FightReferenceType.PROPAGATED,
-          null
+          Some(FightReferenceType.PROPAGATED),
+          None
         ),
-        new CompScoreDTO(null, null, new ScoreDTO(0, 0, 0, Array.empty), 1, FightReferenceType.PROPAGATED, null)
+        CompScore(None, None, Some(Score(0, 0, 0, Array.empty)), 1, Some(FightReferenceType.PROPAGATED), None)
       ),
       600,
       0,
-      false,
+      invalid = false,
       StageRoundType.WINNER_BRACKETS,
       FightStatus.UNCOMPLETABLE,
-      null,
+      None,
       mat1,
-      null,
+      None,
       0,
       competitionId,
-      null,
-      null,
+      None,
+      None,
       stageId,
-      null,
-      null,
-      0
+      None,
+      None
     ),
-    new FightDescriptionDTO(
+    FightDescription(
       fight3,
       categoryId,
-      "Final",
-      null,
-      null,
-      Array(
-        new CompScoreDTO(null, null, new ScoreDTO(0, 0, 0, Array.empty), 0, FightReferenceType.WINNER, fight2),
-        new CompScoreDTO(null, null, new ScoreDTO(0, 0, 0, Array.empty), 1, FightReferenceType.WINNER, fight1)
+      Some("Final"),
+      None,
+      None,
+      Seq(
+        CompScore(None, None, Some(Score(0, 0, 0, Seq.empty)), 0, Some(FightReferenceType.WINNER), Some(fight2)),
+        CompScore(None, None, Some(Score(0, 0, 0, Seq.empty)), 1, Some(FightReferenceType.WINNER), Some(fight1))
       ),
       600,
       1,
-      false,
+      invalid = false,
       StageRoundType.GRAND_FINAL,
       FightStatus.PENDING,
-      null,
-      mat1,
-      null,
-      0,
+      None,
+      Some(mat1),
+      None,
+      None,
       competitionId,
-      periodId,
-      null,
+      Some(periodId),
+      None,
       stageId,
-      null,
-      null,
+      None,
+      None,
       1
     )
   )
