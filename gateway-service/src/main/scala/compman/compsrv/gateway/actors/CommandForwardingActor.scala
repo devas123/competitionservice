23f5ea569c2d813ab0eb8ae6909f430d36039cb3
ggrigori <grigoriy.grigoriev@gmail.com>
2022-07-23 22:53:47 +0100

    Migrated GatewayService

--- gateway-service/src/main/scala/compman/compsrv/gateway/actors/CommandForwardingActor.scala
+++ gateway-service/src/main/scala/compman/compsrv/gateway/actors/CommandForwardingActor.scala
@@ -1,85 +1,72 @@
 package compman.compsrv.gateway.actors
 
+import akka.actor.typed.{ActorRef, Behavior}
+import akka.actor.typed.scaladsl.Behaviors
 import compman.compsrv.logic.actor.kafka.KafkaSupervisor
 import compman.compsrv.logic.actor.kafka.KafkaSupervisor.KafkaSupervisorCommand
-import compman.compsrv.logic.actors.{ActorBehavior, ActorRef, Behaviors}
-import compman.compsrv.logic.actors.ActorSystem.ActorConfig
 import compman.compsrv.CompetitionEventsTopic
 import compman.compsrv.gateway.config.{ConsumerConfig, ProducerConfig}
 import compservice.model.protobuf.callback.CommandCallback
 import compservice.model.protobuf.command.Command
-import zio.{Tag, ZIO}
-import zio.clock.Clock
-import zio.console.Console
-import zio.logging.Logging
 
 object CommandForwardingActor {
 
   sealed trait GatewayApiCommand
 
   final case class ForwardCommand(competitionId: String, body: Array[Byte]) extends GatewayApiCommand
+
   final case class SendCompetitionCommandAndWaitForResult(competitionId: String, body: Array[Byte])(
     val replyTo: ActorRef[CommandCallback]
   ) extends GatewayApiCommand
+
   final case class SendAcademyCommandAndWaitForResult(body: Array[Byte])(val replyTo: ActorRef[CommandCallback])
       extends GatewayApiCommand
 
-  case class ActorState()
-  val initialState: ActorState = ActorState()
-  import Behaviors._
-  def behavior[R: Tag](
-                        kafkaSupervisorActor: ActorRef[KafkaSupervisorCommand],
-                        producerConfig: ProducerConfig,
-                        consumerConfig: ConsumerConfig,
-                        groupId: String,
-                        callbackTimeoutMs: Int
-  ): ActorBehavior[R with Logging with Clock with Console, ActorState, GatewayApiCommand] = Behaviors
-    .behavior[R with Logging with Clock with Console, ActorState, GatewayApiCommand]
-    .withReceive { (ctx, _, state, command, _) =>
-      {
-        for {
-          _ <- Logging.info(s"Received academy API command $command")
-          res <- command match {
-            case _ @ForwardCommand(competitionId, body) => for {
-                _ <- kafkaSupervisorActor ! KafkaSupervisor.PublishMessage(producerConfig.globalCommandsTopic, competitionId, body)
-              } yield state
-            case x: SendCompetitionCommandAndWaitForResult => for {
-                cmd <- ZIO.effect(Command.parseFrom(x.body))
-                _ <- ctx.make(
-                  s"CallbackWaiter-${x.competitionId}-${cmd.messageInfo.flatMap(_.id).getOrElse("")}",
-                  ActorConfig(),
-                  CommandCallbackAggregator.initialState,
-                  CommandCallbackAggregator.behavior(
-                    cmd,
-                    kafkaSupervisorActor,
-                    CompetitionEventsTopic(consumerConfig.eventsTopicPrefix)(x.competitionId),
-                    consumerConfig.callbackTopic,
-                    groupId,
-                    x.replyTo
-                  )(callbackTimeoutMs)
-                )
-                _ <- kafkaSupervisorActor ! KafkaSupervisor.PublishMessage(producerConfig.globalCommandsTopic, x.competitionId, x.body)
-            } yield state
-            case x: SendAcademyCommandAndWaitForResult =>
-              for {
-                cmd <- ZIO.effect(Command.parseFrom(x.body))
-                _ <- ctx.make(
-                  s"AcademyCommandCallbackWaiter-${cmd.messageInfo.flatMap(_.id).getOrElse("")}",
-                  ActorConfig(),
-                  CommandCallbackAggregator.initialState,
-                  CommandCallbackAggregator.behavior(
-                    cmd,
-                    kafkaSupervisorActor,
-                    consumerConfig.academyNotificationsTopic,
-                    consumerConfig.callbackTopic,
-                    groupId,
-                    x.replyTo
-                  )(callbackTimeoutMs)
-                )
-                _ <- kafkaSupervisorActor ! KafkaSupervisor.PublishMessage(producerConfig.academyCommandsTopic, null, x.body)
-              } yield state
-          }
-        } yield res
+  def behavior(
+    kafkaSupervisorActor: ActorRef[KafkaSupervisorCommand],
+    producerConfig: ProducerConfig,
+    consumerConfig: ConsumerConfig,
+    groupId: String,
+    callbackTimeoutMs: Int
+  ): Behavior[GatewayApiCommand] = Behaviors.setup[GatewayApiCommand] { ctx =>
+    Behaviors.receiveMessage { command =>
+      ctx.log.info(s"Received academy API command $command")
+      command match {
+        case _ @ForwardCommand(competitionId, body) =>
+          kafkaSupervisorActor ! KafkaSupervisor.PublishMessage(producerConfig.globalCommandsTopic, competitionId, body)
+          Behaviors.same
+        case x: SendCompetitionCommandAndWaitForResult =>
+          val cmd = Command.parseFrom(x.body)
+          ctx.spawn(
+            CommandCallbackAggregator.behavior(
+              cmd,
+              kafkaSupervisorActor,
+              CompetitionEventsTopic(consumerConfig.eventsTopicPrefix)(x.competitionId),
+              consumerConfig.callbackTopic,
+              groupId,
+              x.replyTo
+            )(callbackTimeoutMs),
+            s"CallbackWaiter-${x.competitionId}-${cmd.messageInfo.flatMap(_.id).getOrElse("")}"
+          )
+          kafkaSupervisorActor !
+            KafkaSupervisor.PublishMessage(producerConfig.globalCommandsTopic, x.competitionId, x.body)
+          Behaviors.same
+        case x: SendAcademyCommandAndWaitForResult =>
+          val cmd = Command.parseFrom(x.body)
+          ctx.spawn(
+            CommandCallbackAggregator.behavior(
+              cmd,
+              kafkaSupervisorActor,
+              consumerConfig.academyNotificationsTopic,
+              consumerConfig.callbackTopic,
+              groupId,
+              x.replyTo
+            )(callbackTimeoutMs),
+            s"AcademyCommandCallbackWaiter-${cmd.messageInfo.flatMap(_.id).getOrElse("")}"
+          )
+          kafkaSupervisorActor ! KafkaSupervisor.PublishMessage(producerConfig.academyCommandsTopic, null, x.body)
+          Behaviors.same
       }
     }
+  }
 }
--- gateway-service/src/main/scala/compman/compsrv/gateway/config/AppConfig.scala
+++ gateway-service/src/main/scala/compman/compsrv/gateway/config/AppConfig.scala
@@ -1,24 +1,31 @@
 package compman.compsrv.gateway.config
 
-import com.typesafe.config.ConfigFactory
-import zio._
-import zio.config.magnolia.DeriveConfigDescriptor
-import zio.config.read
-import zio.config.typesafe.TypesafeConfigSource
+import com.typesafe.config.Config
 
 final case class AppConfig(producer: ProducerConfig, consumer: ConsumerConfig, callbackTimeoutMs: Int)
 
-final case class ConsumerConfig(callbackTopic: String, eventsTopicPrefix: String, groupId: String, academyNotificationsTopic: String)
+final case class ConsumerConfig(
+  callbackTopic: String,
+  eventsTopicPrefix: String,
+  groupId: String,
+  academyNotificationsTopic: String
+)
 
-final case class ProducerConfig(bootstrapServers: String, globalCommandsTopic: String, academyCommandsTopic: String) {
-  def brokers: List[String] = bootstrapServers.split(",").toList
-}
-
+final case class ProducerConfig(bootstrapServers: String, globalCommandsTopic: String, academyCommandsTopic: String)
 object AppConfig {
-  private val descriptor = DeriveConfigDescriptor.descriptor[AppConfig]
 
-  def load(): Task[AppConfig] = for {
-    configSource           <- ZIO(TypesafeConfigSource.fromTypesafeConfig(ZIO.effect(ConfigFactory.load()).map(_.getConfig("gateway"))))
-    config                 <- read(AppConfig.descriptor.from(configSource))
-  } yield config
+  def load(config: Config): AppConfig = AppConfig(
+    producer = ProducerConfig(
+      bootstrapServers = config.getString("gateway.producer.bootstrapServers"),
+      globalCommandsTopic = config.getString("gateway.producer.globalCommandsTopic"),
+      academyCommandsTopic = config.getString("gateway.producer.academyCommandsTopic")
+    ),
+    consumer = ConsumerConfig(
+      callbackTopic = config.getString("gateway.consumer.callbackTopic"),
+      eventsTopicPrefix = config.getString("gateway.consumer.eventsTopicPrefix"),
+      groupId = config.getString("gateway.consumer.groupId"),
+      academyNotificationsTopic = config.getString("gateway.consumer.academyNotificationsTopic")
+    ),
+    callbackTimeoutMs = config.getInt("gateway.callbackTimeoutMs")
+  )
 }
